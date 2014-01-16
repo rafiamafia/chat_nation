428ebfb4e6e8d9308c76e9fe6e3f43a899a3414a
Rafia Qutab <rafiaqutabatx@gmail.com>
2014-01-16 01:59:20 -0800

    complete event model spec'

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ActiveRecord::Base
   validates :event_type, :chat_room, :initiating_user, presence: true
+  validates :comment, presence: true, if: :comment_type?
 
   belongs_to :initiating_user, class_name: 'User'
   belongs_to :receiving_user, class_name: 'User'
   belongs_to :event_type
   belongs_to :chat_room
+
+  def comment_type?
+    self.event_type == EventType.find_by(name: "comment")
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,12 +3,26 @@ require 'spec_helper'
 describe Event do
   context 'validation' do
     it 'must belong to an event_type' do
+      described_class.create.should_not be_valid
+      described_class.create.errors.should have_key :event_type
     end
 
     it 'must have a chat room' do
+      described_class.create.should_not be_valid
+      described_class.create.errors.should have_key :chat_room
     end
 
     it 'must have an initiating user' do
+      described_class.create.should_not be_valid
+      described_class.create.errors.should have_key :initiating_user
+    end
+
+    it 'comment attribute must exist if a comment type' do
+      subject = described_class.create initiating_user: User.new,
+                                       chat_room: ChatRoom.new,
+                                       event_type: EventType.find_by(name: 'comment')
+      subject.valid?.should == false
+      subject.errors.should have_key :comment
     end
   end
 end
