a0f5fa0cc44b80e02af1d614a21ee8ffd828ba70
Rafia Qutab <rafiaqutabatx@gmail.com>
2014-01-16 11:08:59 -0800

    work on this rspec with Dan - created test data in console before hand'

--- spec/lib/data_aggregator_spec.rb
+++ spec/lib/data_aggregator_spec.rb
@@ -6,8 +6,27 @@ describe DataAggregator do
     let(:room) { ChatRoom.create title: "testing", initiating_user: user }
 
     it 'takes a chat_room and granuality_level' do
-      described_class.should_receive(:group_by).with(room, GranualityLevel::MINUTE)
-      DataAggregator.group_by room, GranualityLevel::MINUTE
+      described_class.should_receive(:group_by).with(room, GranularityLevel::MINUTE)
+      DataAggregator.group_by room, GranularityLevel::MINUTE
+    end
+
+    context 'presentation of data aggregation' do
+      context 'GranularityLevel::MINUTE' do
+      end
+
+      context 'GranularityLevel::HOUR' do
+      end
+
+      context 'GranularityLevel::DAY' do
+      end
+
+      context 'GranularityLevel::WEEK' do
+      end
+      context 'GranularityLevel::MONTH' do
+      end
+
+      context 'GranularityLevel::YEAR' do
+      end
     end
   end
 end
